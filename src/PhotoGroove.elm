73bbb43dbb87d7d59b83400c85fb33ecd9e367b0
xuyanwen2012 <info@chivalrystudio.com>
2020-04-21 22:09:51 -0700

    demo hello world

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -1,7 +1,9 @@
 module PhotoGroove exposing (hum)
 
-import Html exposing (div, h1, img, text)
+import Browser
+import Html exposing (Html, div, h1, img, text)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
 
 
 urlPrefix : String
@@ -9,7 +11,47 @@ urlPrefix =
     "http://elm-in-action.com/"
 
 
-view : { photos : List { url : String }, selectedUrl : String } -> Html.Html msg
+
+---- MODEL ----
+
+
+type alias Model =
+    { photos : List { url : String }, selectedUrl : String }
+
+
+init : Model
+init =
+    { photos =
+        [ { url = "1.jpeg" }
+        , { url = "2.jpeg" }
+        , { url = "3.jpeg" }
+        ]
+    , selectedUrl = "1.jpeg"
+    }
+
+
+
+---- UPDATE ----
+--type alias Msg =
+--    { description : String, data : String }
+
+
+type Msg
+    = ImgClick String
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        ImgClick data ->
+            { model | selectedUrl = data }
+
+
+
+---- VIEW ----
+
+
+view : Model -> Html Msg
 view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
@@ -26,39 +68,25 @@ view model =
         ]
 
 
-viewThumbnail : String -> { url : String } -> Html.Html msg
+viewThumbnail : String -> { url : String } -> Html Msg
 viewThumbnail selectedUrl thumb =
     img
         [ src (urlPrefix ++ thumb.url)
         , classList
             [ ( "selected", selectedUrl == thumb.url )
             ]
+        , onClick (ImgClick thumb.url)
         ]
         []
 
 
 
---if selectedUrl == thumb.url then
---    img
---        [ src (urlPrefix ++ thumb.url)
---        , class "selected"
---        ]
---        []
---
---else
---    img [ src (urlPrefix ++ thumb.url) ] []
-
-
-initialModel : { photos : List { url : String }, selectedUrl : String }
-initialModel =
-    { photos =
-        [ { url = "1.jpeg" }
-        , { url = "2.jpeg" }
-        , { url = "3.jpeg" }
-        ]
-    , selectedUrl = "1.jpeg"
-    }
+---- PROGRAM ----
 
 
 hum =
-    view initialModel
+    Browser.sandbox
+        { init = init
+        , view = view
+        , update = update
+        }
