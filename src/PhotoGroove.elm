c327e304edf5950a38e6d2cedb195fe344375881
xuyanwen2012 <info@chivalrystudio.com>
2020-04-21 21:34:18 -0700

    update model

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -9,25 +9,55 @@ urlPrefix =
     "http://elm-in-action.com/"
 
 
-view : List { url : String } -> Html.Html msg
+view : { photos : List { url : String }, selectedUrl : String } -> Html.Html msg
 view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
-        , div [ id "thumbnails" ] (List.map viewThumbnail model)
+        , div [ id "thumbnails" ]
+            (List.map
+                (\photo -> viewThumbnail model.selectedUrl photo)
+                model.photos
+            )
+        , img
+            [ class "large"
+            , src (urlPrefix ++ "large/" ++ model.selectedUrl)
+            ]
+            []
         ]
 
 
-viewThumbnail : { url : String } -> Html.Html msg
-viewThumbnail thumb =
-    img [ src (urlPrefix ++ thumb.url) ] []
+viewThumbnail : String -> { url : String } -> Html.Html msg
+viewThumbnail selectedUrl thumb =
+    img
+        [ src (urlPrefix ++ thumb.url)
+        , classList
+            [ ( "selected", selectedUrl == thumb.url )
+            ]
+        ]
+        []
 
 
-initialModel : List { url : String }
+
+--if selectedUrl == thumb.url then
+--    img
+--        [ src (urlPrefix ++ thumb.url)
+--        , class "selected"
+--        ]
+--        []
+--
+--else
+--    img [ src (urlPrefix ++ thumb.url) ] []
+
+
+initialModel : { photos : List { url : String }, selectedUrl : String }
 initialModel =
-    [ { url = "1.jpeg" }
-    , { url = "2.jpeg" }
-    , { url = "3.jpeg" }
-    ]
+    { photos =
+        [ { url = "1.jpeg" }
+        , { url = "2.jpeg" }
+        , { url = "3.jpeg" }
+        ]
+    , selectedUrl = "1.jpeg"
+    }
 
 
 hum =
