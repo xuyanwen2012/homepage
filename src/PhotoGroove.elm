549e79707833dd083ac6e07d11b6b19dcff95e99
xuyanwen2012 <info@chivalrystudio.com>
2020-04-21 22:42:42 -0700

    add button

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -1,7 +1,8 @@
 module PhotoGroove exposing (hum)
 
+import Array exposing (Array)
 import Browser
-import Html exposing (Html, div, h1, img, text)
+import Html exposing (Html, button, div, h1, img, text)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
 
@@ -16,7 +17,11 @@ urlPrefix =
 
 
 type alias Model =
-    { photos : List { url : String }, selectedUrl : String }
+    { photos : List Photo, selectedUrl : String }
+
+
+type alias Photo =
+    { url : String }
 
 
 init : Model
@@ -30,12 +35,18 @@ init =
     }
 
 
+photoArray : Array Photo
+photoArray =
+    Array.fromList init.photos
+
+
 
 ---- UPDATE ----
 
 
 type Msg
     = ImgClick String
+    | ButtonClick
 
 
 update : Msg -> Model -> Model
@@ -43,6 +54,9 @@ update msg model =
     case msg of
         ImgClick data ->
             { model | selectedUrl = data }
+
+        ButtonClick ->
+            { model | selectedUrl = "2.jpeg" }
 
 
 
@@ -53,6 +67,9 @@ view : Model -> Html Msg
 view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
+        , button
+            [ onClick ButtonClick ]
+            [ text "Surprise  Me!" ]
         , div [ id "thumbnails" ]
             (List.map
                 (viewThumbnail model.selectedUrl)
@@ -66,7 +83,7 @@ view model =
         ]
 
 
-viewThumbnail : String -> { url : String } -> Html Msg
+viewThumbnail : String -> Photo -> Html Msg
 viewThumbnail selectedUrl thumb =
     img
         [ src (urlPrefix ++ thumb.url)
