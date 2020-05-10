4df75f2c2151e0a642555041b187c5fcd19c2cc4
xuyanwen2012 <info@chivalrystudio.com>
2020-05-09 17:55:05 -0700

    folders

--- src/PhotoFolders.elm
+++ src/PhotoFolders.elm
@@ -34,13 +34,35 @@ type alias Photo =
 type alias Model =
     { selectedUrl : Maybe String
     , photos : Dict String Photo
+    , root : Folder
     }
+
+
+type Folder
+    = Folder
+        { name : String
+        , expanded : Bool
+        , photoUrls : List String
+        , subfolders : List Folder
+        }
+
+
+type FolderPath
+    = End
+    | Subfolder Int FolderPath
 
 
 initialModel : Model
 initialModel =
     { selectedUrl = Nothing
     , photos = Dict.empty
+    , root =
+        Folder
+            { name = "Loading.."
+            , expanded = True
+            , photoUrls = []
+            , subfolders = []
+            }
     }
 
 
@@ -61,6 +83,7 @@ init _ =
 type Msg
     = ClickedPhoto String
     | GotInitialModel (Result Http.Error Model)
+    | ClickedFolder FolderPath
 
 
 modelDecoder : Decoder Model
@@ -91,6 +114,52 @@ modelDecoder =
                     }
                   )
                 ]
+        , root =
+            Folder
+                { name = "Photos"
+                , expanded = True
+                , photoUrls = []
+                , subfolders =
+                    [ Folder
+                        { name = "2016"
+                        , expanded = True
+                        , photoUrls = [ "trevi", "coli" ]
+                        , subfolders =
+                            [ Folder
+                                { name = "outdoors"
+                                , expanded = True
+                                , photoUrls = []
+                                , subfolders = []
+                                }
+                            , Folder
+                                { name = "indoors"
+                                , expanded = True
+                                , photoUrls = [ "fresco" ]
+                                , subfolders = []
+                                }
+                            ]
+                        }
+                    , Folder
+                        { name = "2017"
+                        , expanded = True
+                        , photoUrls = []
+                        , subfolders =
+                            [ Folder
+                                { name = "outdoors"
+                                , expanded = True
+                                , photoUrls = []
+                                , subfolders = []
+                                }
+                            , Folder
+                                { name = "indoors"
+                                , expanded = True
+                                , photoUrls = []
+                                , subfolders = []
+                                }
+                            ]
+                        }
+                    ]
+                }
         }
 
 
@@ -109,6 +178,32 @@ update msg model =
 
         GotInitialModel (Err _) ->
             ( model, Cmd.none )
+
+        ClickedFolder path ->
+            ( { model | root = toggleExpanded path model.root }, Cmd.none )
+
+
+toggleExpanded : FolderPath -> Folder -> Folder
+toggleExpanded path (Folder folder) =
+    case path of
+        End ->
+            Folder { folder | expanded = not folder.expanded }
+
+        Subfolder targetIndex remainingPath ->
+            let
+                subfolders : List Folder
+                subfolders =
+                    List.indexedMap transform folder.subfolders
+
+                transform : Int -> Folder -> Folder
+                transform currentIndex currentSubfolder =
+                    if currentIndex == targetIndex then
+                        toggleExpanded remainingPath currentSubfolder
+
+                    else
+                        currentSubfolder
+            in
+            Folder { folder | subfolders = subfolders }
 
 
 
@@ -131,7 +226,11 @@ view model =
                     text ""
     in
     div [ class "content" ]
-        [ div [ class "selected-photo" ] [ selectedPhoto ]
+        [ div [ class "folders" ]
+            [ h1 [] [ text "Folders" ]
+            , viewFolder End model.root
+            ]
+        , div [ class "selected-photo" ] [ selectedPhoto ]
         ]
 
 
@@ -156,6 +255,48 @@ viewRelatedPhoto url =
         , src (urlPrefix ++ "photos/" ++ url ++ "/thumb")
         ]
         []
+
+
+viewPhoto : String -> Html Msg
+viewPhoto url =
+    div [ class "photo", onClick (ClickedPhoto url) ]
+        [ text url ]
+
+
+viewFolder : FolderPath -> Folder -> Html Msg
+viewFolder path (Folder folder) =
+    let
+        viewSubfolder : Int -> Folder -> Html Msg
+        viewSubfolder index subfolder =
+            viewFolder (appendIndex index path) subfolder
+
+        folderLabel =
+            label [ onClick (ClickedFolder path) ] [ text folder.name ]
+    in
+    if folder.expanded then
+        let
+            contents =
+                List.append
+                    (List.indexedMap viewSubfolder folder.subfolders)
+                    (List.map viewPhoto folder.photoUrls)
+        in
+        div [ class "folder expanded" ]
+            [ folderLabel
+            , div [ class "contents" ] contents
+            ]
+
+    else
+        div [ class "folder collapsed" ] [ folderLabel ]
+
+
+appendIndex : Int -> FolderPath -> FolderPath
+appendIndex index path =
+    case path of
+        End ->
+            Subfolder index End
+
+        Subfolder subfolderIndex remainingPath ->
+            Subfolder subfolderIndex (appendIndex index remainingPath)
 
 
 
